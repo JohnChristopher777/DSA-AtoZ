50cf76e06edab33e282f6a2099c8467d1795c719
JohnChristopher777 <johnchristopher123net@gmail.com>
2026-07-22 12:14:03 +0530

    Add gitignore and clean build artifacts

--- src/hashing/charhash.java
+++ src/hashing/charhash.java
@@ -5,22 +5,23 @@ import java.util.Scanner;
 //time complexity - O(Q+N)
 public class charhash {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter a string: ");
-        String str = s.nextLine();
-        
-        //precompute
-        int[] hash = new int[256];
-        for (int i = 0; i < str.length(); i++) {
-            hash[str.charAt(i)]++; //hash[str.charAt(i) - 'a']++; or hash[str.charAt(i) - 'A'] if only upper or samaller case
-        }
-        
-        System.out.println("Enter the number of queries: ");
-        int q = s.nextInt();
-        while (q-- > 0) {
-            char c = s.next().charAt(0);
+        try (Scanner s = new Scanner(System.in)) {
+            System.out.println("Enter a string: ");
+            String str = s.nextLine();
+            
+            //precompute
+            int[] hash = new int[256];
+            for (int i = 0; i < str.length(); i++) {
+                hash[str.charAt(i)]++; //hash[str.charAt(i) - 'a']++; or hash[str.charAt(i) - 'A'] if only upper or samaller case
+            }
+            
+            System.out.println("Enter the number of queries: ");
+            int q = s.nextInt();
+            while (q-- > 0) {
+                char c = s.next().charAt(0);
 
-            System.out.println(c+" appears " + hash[c] + " times in the given string."); //hash[c-'a'] or [c - 'A']
+                System.out.println(c+" appears " + hash[c] + " times in the given string."); //hash[c-'a'] or [c - 'A']
+            }
         }
     }
 }
--- src/hashing/freqhash.java
+++ src/hashing/freqhash.java
@@ -27,17 +27,18 @@ public class freqhash {
     }
 
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
+        try (Scanner s = new Scanner(System.in)) {
+            int n = s.nextInt();
 
-        int[] arr = new int[n];
+            int[] arr = new int[n];
 
-        for (int i = 0; i < n; i++) {
-            arr[i] = s.nextInt();
+            for (int i = 0; i < n; i++) {
+                arr[i] = s.nextInt();
+            }
+
+            freqhash obj = new freqhash();
+            obj.freqq(arr, n);
         }
-
-        freqhash obj = new freqhash();
-        obj.freqq(arr, n);
 
     }
 }
